6c025853b82cda05e850bef402abf06839b9cabd
AyushM2206 <ayushmanyania12@gmail.com>
2025-04-02 19:52:46 +0530

    "updated"

--- a.py
+++ a.py
@@ -1,7 +1,9 @@
 
-num1 = 10
-num2 = 5
-sum = num1 + num2
-print("The sum is:", sum)
-print("AYUSH")
-print("i read in cu")
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+
+
+sum = float(num1) + float(num2)
+
+
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
